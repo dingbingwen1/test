fca02e1453256220fe2aca531f9fa1d3b0b93543
dbw <354863199@qq.com>
2019-10-03 03:41:42 -0700

    test1

---
+++ test1/mysort.c
@@ -0,0 +1,25 @@
+#include "uhead.h"
+void msort(int * base,int n)
+{
+	int i,j,t;
+	for(i=0;i<n-1;i++)
+	{
+   		for(j=0;j<n-1;j++)
+		{
+			if(base[j]>base[j+1]){
+				t=base[j];
+				base[j]=base[j+1];
+				base[j+1]=t;
+			}
+		}
+	}
+}
+void show(int *base,int n)
+{
+	int i;
+	for(i=0;i<n;i++)
+	{
+	printf("%d",base[i]);
+	printf("\n");
+	}
+}
---
+++ test1/qsort.c
@@ -0,0 +1,12 @@
+#include "uhead.h"
+int compar (const void *a,const void *b)
+{
+	int *aa=(int *)a,*bb=(int *)b;
+	if(*aa > *bb) return 1;
+	if(*aa == *bb) return 0;
+	if(*aa < *bb) return -1;
+}
+void qsortt(int *base,int n)
+{
+	qsort(base,10,sizeof(int),compar);
+}
